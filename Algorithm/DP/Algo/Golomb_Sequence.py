ecb3e998847332b30ccc526a8b15c6416787c1a7
shishir-kr92 <shishir.kr92@gmail.com>
2019-03-05 23:19:56 +0530

    HackerRankCode

---
+++ Algorithm/DP/Algo/Golomb_Sequence.py
@@ -0,0 +1,16 @@
+
+def get_golomb_sequence(n):
+    golomb_sequence = [0 for i in range(n+1)]
+    golomb_sequence[1] = 1
+
+    for i in range(2, n+1):
+        golomb_sequence[i] = 1 + golomb_sequence[ i - golomb_sequence[golomb_sequence[i-1]]]
+
+    return golomb_sequence[1:-1]
+
+
+
+if __name__ == '__main__':
+    n  = int(input().strip())
+    print(get_golomb_sequence(n))
+
---
+++ Algorithm/DP/Recurcive/Golomb_Sequence.py
@@ -0,0 +1,21 @@
+
+
+def find_golomb(n):
+    if n ==1:
+        return 1
+
+    return 1 + find_golomb(n - find_golomb(find_golomb(n-1)))
+
+
+def get_golomb_sequence(n):
+    golomb_no_list = [0 for i in range(n+1)]
+    golomb_no_list[1]  = 1
+    for i in range(2, n+1):
+        no = find_golomb(i)
+        golomb_no_list[i] = no
+    return golomb_no_list
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    print(get_golomb_sequence(n))
